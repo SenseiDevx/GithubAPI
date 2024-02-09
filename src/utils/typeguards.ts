0f53c1aca7cd7acd2af3bde15c7d0c02c5492400
Omurbek Nurlanbekovich <122143128+SenseiDevx@users.noreply.github.com>
2024-02-09 23:20:49 +0600

    Update typeguards.ts

--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -1,3 +1,3 @@
 import {GithubUser} from "../types";
 
-export const isGithubUser = (user: any): user is GithubUser => 'id' in user; 
+export const isGithubUser = (user: any): user is GithubUser => 'id' in user
